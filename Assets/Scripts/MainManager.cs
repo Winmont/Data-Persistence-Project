3c9acf358f01cd96f62801f92390389c6a661ee1
Winmont <106991316+Winmont@users.noreply.github.com>
2022-06-07 10:15:18 +0200

    changed more on Mainmanger
    Changed MainManger script but noticed its prob  easier to make entirly new script because main manager also manages load of game objects

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 public class MainManager : MonoBehaviour
 {
     public static MainManager instance;
+    [SerializeField] private InputField input;
+    [SerializeField] private GameObject paddle;
 
     public string playerName;
-    public int highscore;
+    public int highscore = 0;
 
     public Brick BrickPrefab;
     public int LineCount = 6;
@@ -40,42 +46,32 @@ public class MainManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
-        {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
-        }
+            UpdateInputField();
     }
 
     private void Update()
     {
-        if (!m_Started)
+        if (SceneManager.GetActiveScene().buildIndex == 1)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (!m_Started)
             {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    m_Started = true;
+                    float randomDirection = Random.Range(-1.0f, 1.0f);
+                    Vector3 forceDir = new Vector3(randomDirection, 1, 0);
+                    forceDir.Normalize();
 
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
+                    Ball.transform.SetParent(null);
+                    Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
+                }
             }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
+            else if (m_GameOver)
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                }
             }
         }
     }
@@ -118,6 +114,52 @@ public class MainManager : MonoBehaviour
 
             name = data.name;
             highscore = data.highscore;
+        }
+    }
+
+    public void UpdateName()
+    {
+        name = input.text;
+    }
+
+    private void UpdateInputField()
+    {
+        input.text = name;
+    }
+
+    public void Exit()
+    {
+        SaveHighscoreAndName();
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+
+        Application.Quit();
+#endif
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+        SetupGame();
+        paddle.SetActive(true);
+    }
+
+    private void SetupGame()
+    {
+        const float step = 0.6f;
+        int perLine = Mathf.FloorToInt(4.0f / step);
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
+        for (int i = 0; i < LineCount; ++i)
+        {
+            for (int x = 0; x < perLine; ++x)
+            {
+                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                brick.PointValue = pointCountArray[i];
+                brick.onDestroyed.AddListener(AddPoint);
+            }
         }
     }
 }
